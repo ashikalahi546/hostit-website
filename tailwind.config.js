a66fbb862242e102ce93cb0aa4fc41a9f0226477
Md.Ashik <ashikalahi546@gmail.com>
2024-01-06 15:44:04 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#020230',
+        secondary:'#020230',
+      }
+    },
+  },
+  plugins: [],
+}
+
